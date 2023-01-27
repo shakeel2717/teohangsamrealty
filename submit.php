27359df8401526684208f7f46ff4951dfeb13686
Shakeel Ahmad <shakeel2717@gmail.com>
2023-01-27 01:55:49 -0800

    Store Data in TXT File

--- submit.php
+++ submit.php
@@ -6,6 +6,8 @@ $contact_details = $_POST['contact_details'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 if (isset($_POST['name']) && isset($_POST['email'])) {
+    // storing this data to a file
+    $file = fopen("form_data.txt", "a") or die("Unable to open file!");
     $to = "shakeel2717@gmail.com";
     $subject = "Form Submission";
     $message = "Name: " . $_POST['name'] . "\n";
@@ -13,6 +15,8 @@ if (isset($_POST['name']) && isset($_POST['email'])) {
     $message .= "Contact: " . $_POST['contact_details'] . "\n";
     $message .= "Company: " . $_POST['contact_details'] . "\n";
     $message .= "Message: " . $_POST['message'] . "\n";
+    fwrite($file, $message);
+    fclose($file);
     $headers = "From: " . $_POST['email'];
     mail($to, $subject, $message, $headers);
     echo "Form submitted successfully!";
